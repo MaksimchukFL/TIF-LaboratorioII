f30641d4419cd016227ab12b6b4b6721fcbfa2d8
MaksimchukFL <francolucastillo@gmail.com>
2022-11-17 23:21:03 -0300

    Clases Opcion y EvaluarIngreso

--- src/EvaluarIngreso.java
+++ src/EvaluarIngreso.java
@@ -1,2 +1,17 @@
-public class EvaluarIngreso {
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public final class EvaluarIngreso {
+
+    public static int deEntero(int n){
+        Scanner leer = new Scanner(System.in);
+        try {
+            n = leer.nextInt();
+        }catch (InputMismatchException e){
+            System.out.println("\nERROR EN EL INGRESO DE DATOS");
+            n = 0;
+        }
+
+        return  n;
+    }
 }
--- src/Opcion.java
+++ src/Opcion.java
@@ -1,2 +1,25 @@
 public class Opcion {
+    private String texto;
+    private boolean buenoOMalo;
+
+    public Opcion(String texto,boolean buenoOMalo){
+        this.texto = texto;
+        this.buenoOMalo = buenoOMalo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public boolean isBuenoOMalo() {
+        return buenoOMalo;
+    }
+
+    public void setBuenoOMalo(boolean buenoOMalo) {
+        this.buenoOMalo = buenoOMalo;
+    }
 }
