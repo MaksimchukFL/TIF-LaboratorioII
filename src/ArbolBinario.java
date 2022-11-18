3b634289e261aed1069f676cde7cc00209782341
Matias Barboza <matiasbarboza540@gmail.com>
2022-11-17 22:49:01 -0300

    Estructura de arbol y nodo

--- src/ArbolBinario.java
+++ src/ArbolBinario.java
@@ -1,2 +1,76 @@
 public class ArbolBinario {
+
+    private Nodo raiz;
+    private Nodo actual;
+
+    public ArbolBinario(){
+        this.raiz = null;
+    }
+
+    public Nodo getRaiz(){
+        return this.raiz;
+    }
+
+    public Nodo getActual() {
+        return actual;
+    }
+
+    public void setActual(Nodo actual) {
+        this.actual = actual;
+    }
+
+    public boolean vacio(){
+        return this.raiz == null;
+    }
+
+    public void insertar(Nodo nuevo){
+
+        if(vacio()){
+            this.raiz = nuevo;
+            this.actual = raiz;
+        }else{
+
+            Nodo temporal = raiz;
+
+            while (temporal != null){
+
+                nuevo.setPadre(temporal);
+
+                if(nuevo.getLlave() >= temporal.getLlave()){
+                    temporal = temporal.getHijoDerecha();
+                }else{
+                    temporal = temporal.getHijoIzquierda();
+                }
+            }
+
+            if(nuevo.getLlave() < nuevo.getPadre().getLlave()){
+                nuevo.getPadre().setHijoIzquierda(nuevo);
+            }else{
+                nuevo.getPadre().setHijoDerecha(nuevo);
+            }
+        }
+    }
+
+    public void avanzar(int opcion){
+
+        Nodo temporal = actual;
+
+        if(temporal.valorOpcion(opcion)){
+            if(temporal.getHijoDerecha().isValor()){
+                this.actual = temporal.getHijoDerecha();
+            }else if(temporal.getHijoIzquierda().isValor()){
+                this.actual = temporal.getHijoIzquierda();
+            }
+        }else if(!temporal.valorOpcion(opcion)){
+            if(!temporal.getHijoIzquierda().isValor()){
+                this.actual = temporal.getHijoIzquierda();
+            }else if(!temporal.getHijoDerecha().isValor()){
+                this.actual = temporal.getHijoDerecha();
+            }
+        }
+    }
+
+    public void volverARaiz(){
+        setActual(raiz);
+    }
 }
--- src/Nodo.java
+++ src/Nodo.java
@@ -1,2 +1,101 @@
 public class Nodo {
+
+    private Nodo padre;
+    private Nodo hijoDerecha; //Siempre debe ser mayor o igual que la llave
+    private Nodo hijoIzquierda; //Siempre debe ser menor que la llave
+
+    private int llave;
+    private String historia;
+    private Opcion[] opciones;
+    private boolean valor;
+
+
+    public Nodo(int llave,String historia,Opcion[] opciones,boolean valor){
+        this.padre = null;
+        this.hijoDerecha = null;
+        this.hijoIzquierda = null;
+        this.llave = llave;
+        this.historia = historia;
+        this.opciones = opciones;
+        this.valor = valor;
+    }
+
+    public Nodo(int llave,String historia,boolean valor){
+        this.padre = null;
+        this.hijoDerecha = null;
+        this.hijoIzquierda = null;
+        this.llave = llave;
+        this.historia = historia;
+        this.opciones = null;
+        this.valor = valor;
+    }
+
+    public Nodo getPadre() {
+        return padre;
+    }
+
+    public void setPadre(Nodo padre) {
+        this.padre = padre;
+    }
+
+    public Nodo getHijoDerecha() {
+        return hijoDerecha;
+    }
+
+    public void setHijoDerecha(Nodo hijoDerecha) {
+        this.hijoDerecha = hijoDerecha;
+    }
+
+    public Nodo getHijoIzquierda() {
+        return hijoIzquierda;
+    }
+
+    public void setHijoIzquierda(Nodo hijoIzquierda) {
+        this.hijoIzquierda = hijoIzquierda;
+    }
+
+    public int getLlave() {
+        return llave;
+    }
+
+    public void setLlave(int llave) {
+        this.llave = llave;
+    }
+
+    public String getHistoria() {
+        return historia;
+    }
+
+    public void setHistoria(String historia) {
+        this.historia = historia;
+    }
+
+    public Opcion[] getOpciones() {
+        return opciones;
+    }
+
+    public void setOpciones(Opcion[] opciones) {
+        this.opciones = opciones;
+    }
+
+    public boolean valorOpcion(int opcion){
+        return opciones[opcion-1].isBuenoOMalo();
+    }
+
+    public boolean isValor() {
+        return valor;
+    }
+
+    public String lasOpcionesToString(){
+
+        int i=1;
+        String lasOpciones = "";
+
+        for(Opcion opcion:opciones){
+            lasOpciones += i + "- " + opcion.getTexto() + "\n";
+            i++;
+        }
+
+        return lasOpciones;
+    }
 }
